763ac8a9395f65c3c693ad7d8a1ddc59d51b719f
Ben Snowball <bss16@ic.ac.uk>
2016-09-12 15:00:23 +0100

    py file to read in xy.dat and produce plot

---
+++ src/readxy.py
@@ -0,0 +1,49 @@
+import pylab
+import numpy as np
+
+#open and read in the file 
+xydat_file = open("../data/xy.dat", "r")
+
+# The file has data from line 1 onwards, with two collumns
+
+# Set empty lists for storing x and y data (x is first, y second in each row)
+xdat = []
+ydat = []
+
+# Note the number of data points
+n = 0
+
+while True:
+
+    # Use a try/except block to navigate the lines and exit when at the end
+
+    try:
+        line = xydat_file.readline()
+        words = line.split()
+
+        # if we have not got two words, we have an unwanted line so exit
+        if len(words) != 2:
+            break
+
+    except:
+        break
+
+    # Add 1 to the number of data points seen
+    n += 1
+
+    
+    # Add data to each list
+    x = float(words[0])
+    y = float(words[1]) 
+    xdat.append(x)
+    ydat.append(y)
+
+# Plot y against x, but we need to change lists to arrays first
+xdat = np.array(xdat)
+ydat = np.array(ydat)
+pylab.plot(xdat, ydat)
+pylab.xlabel("x")
+pylab.ylabel("y")
+pylab.show()
+
+
